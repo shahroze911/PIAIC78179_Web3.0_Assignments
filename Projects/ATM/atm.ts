8e686df504f99a8353e29fd480c323a7c7115fac
Shahroze Kamran Sahotra <f178035@nu.edu.pk>
2023-08-05 11:36:01 +0500

    Changes Uploaded

--- Projects/ATM/atm.ts
+++ Projects/ATM/atm.ts
@@ -1,1 +1,111 @@
-console.log("Shah");
+import inquirer from "inquirer";
+
+interface User{
+    id:number;
+    password:number;
+}
+interface Operations{
+    accountType:string;
+    transactionType:string;
+    amount:number;
+}
+const user_info:User[]=[
+    {
+        id:1000,
+        password:123456
+    },
+    {
+        id:1001,
+        password:12345
+    }   
+];
+
+async function login(){
+    const questions=[
+        {
+            type:'number',
+            name:'userId',
+            message:'Enter your User ID'
+        },
+        {
+            type:'number',
+            name:'passId',
+            message:'Enter your Password'                        
+        }
+    ];
+    const answers=await inquirer.prompt(questions);
+    const userfind=user_info.find((u)=>u.id===answers.userId);
+    if(!userfind){
+        console.log("User not Found!");
+        return;
+    }
+    
+    if(user_info.find((p)=>p.password===answers.passId)){
+        console.log("Login Successful");
+        const options:Operations=await inquirer.prompt([
+            {
+                type:'list',
+                name:'accountType',
+                choices:["Current","Saving"],
+                message:"Select your Account Type",
+
+            },
+            {
+                type:'list',
+                name:'transactionType',
+                choices:["Fast Cash","Withdraw"],
+                message:"Select your Transaction Type",
+                when(options){
+                    return options.accountType
+                    
+                }                                            
+            },
+            {
+                type:'list',
+                name:'amount',
+                choices:[1000,2000,5000,10000],
+                message:"Select your Amount",
+                when(options){
+                    return options.transactionType==="Fast Cash"
+                }
+            },
+            {
+                type:'number',
+                name:'amount',                
+                message:"Enter your Amount",
+                when(options){
+                    return options.transactionType==="Withdraw"
+                }
+            }
+            
+           
+        ]);
+        const balance=10000;
+        console.log(balance);
+
+        const enteredAmount=options.amount;
+        console.log(enteredAmount);
+        if(enteredAmount<balance){
+            const remainingBalance=balance-enteredAmount;
+            console.log(`${userfind.id} remaining balance is ${remainingBalance}`);
+        }
+
+        
+        
+        
+        
+
+    }
+    
+
+    else{
+        console.log("Login Failed");
+    }
+
+    
+}
+
+
+
+
+login();
