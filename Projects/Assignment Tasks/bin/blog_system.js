afa1b399909dc99539613681c7f6bde87d826975
Shahroze Kamran Sahotra <sksahotra911@gmail.com>
2023-09-02 09:27:02 +0500

    All 3 tasks have been completed

---
+++ Projects/Assignment Tasks/bin/blog_system.js
@@ -0,0 +1,87 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const authors = [
+    {
+        id: 1,
+        name: "Shah",
+        bio: "Software Developer",
+    },
+    {
+        id: 2,
+        name: "Sahotra",
+        bio: "Business Analyst",
+    },
+    {
+        id: 3,
+        name: "Kamran",
+        bio: "Designer",
+    },
+];
+const post = [
+    {
+        post_id: 1,
+        title: "Benefits of Typescript",
+        content: "Content of post 1",
+        author: authors[0],
+        comments: [
+            {
+                author: authors[1],
+                content: "Informative",
+                timestamp: new Date(),
+            },
+            {
+                author: authors[2],
+                content: "Nice article",
+                timestamp: new Date(),
+            },
+        ],
+    },
+    {
+        post_id: 2,
+        title: "What is Metaverse?",
+        content: "Content of post 1",
+        author: authors[1],
+        comments: [
+            {
+                author: authors[0],
+                content: "Mind Blowing",
+                timestamp: new Date(),
+            },
+            {
+                author: authors[1],
+                content: "Pakistan Zindabad",
+                timestamp: new Date(),
+            },
+            {
+                author: authors[2],
+                content: "Nice",
+                timestamp: new Date(),
+            },
+        ],
+    },
+];
+// Function to sort posts based on the number of comments
+function sortPostsByComments(posts) {
+    return posts
+        .slice()
+        .sort((postA, postB) => postB.comments.length - postA.comments.length);
+}
+// Sort the posts
+const sortedPosts = sortPostsByComments(post);
+console.log("Posts Sorted on the basis of number of Comments");
+sortedPosts.forEach((post, postIndex) => {
+    console.log(`Post ID: ${post.post_id}`);
+    console.log(`Post ${postIndex + 1}: `);
+    console.log(`Title ${post.title}`);
+    console.log(`Author ${post.author.name} : ${post.author.bio}`);
+    console.log(`Content ${post.content}`);
+    console.log(`No of Comments: ${post.comments.length} comments`);
+    console.log(`Comments`);
+    post.comments.forEach((comment, commentIndex) => {
+        console.log(`  Comment ${commentIndex + 1}:`);
+        console.log(`  Author: ${comment.author.name}`);
+        console.log(`  Content: ${comment.content}`);
+        console.log(`  Timestamp: ${comment.timestamp}`);
+    });
+    console.log("========================");
+});
---
+++ Projects/Assignment Tasks/bin/social_media.js
@@ -0,0 +1,109 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// List of users
+const users = [
+    { id: 1, username: "zohaib302" },
+    { id: 2, username: "shah007" },
+    { id: 3, username: "naveed7" },
+    { id: 4, username: "hamza4" },
+    { id: 5, username: "sameer5" },
+    { id: 6, username: "ali8" },
+];
+const comments = [
+    {
+        id: 1,
+        user: users[0],
+        content: "Nice picture",
+        timestamp: new Date(),
+    },
+    {
+        id: 2,
+        user: users[1],
+        content: "Great Job",
+        timestamp: new Date(),
+    },
+    {
+        id: 3,
+        user: users[0],
+        content: "😂",
+        timestamp: new Date(),
+    },
+];
+const posts = [
+    {
+        post_id: 1,
+        title: "Feeling Happy",
+        content: "Test Content",
+        likes: [
+            { user: users[0], timestamp: new Date() },
+            { user: users[1], timestamp: new Date() },
+            { user: users[3], timestamp: new Date() },
+            { user: users[2], timestamp: new Date() },
+        ],
+        author: users[0],
+        comments: [comments[0], comments[1]], // Add comments to the post
+    },
+    {
+        post_id: 2,
+        title: "Travelling",
+        content: "Travelling to Skardu",
+        likes: [
+            { user: users[0], timestamp: new Date() },
+            { user: users[1], timestamp: new Date() },
+            { user: users[2], timestamp: new Date() },
+            { user: users[3], timestamp: new Date() },
+            { user: users[4], timestamp: new Date() },
+            { user: users[5], timestamp: new Date() },
+        ],
+        author: users[1],
+        comments: [comments[2], comments[0], comments[1]], // Add comments to the post
+    },
+    {
+        post_id: 3,
+        title: "Going to University",
+        content: "Air University",
+        likes: [
+            { user: users[0], timestamp: new Date() },
+            { user: users[1], timestamp: new Date() },
+            { user: users[4], timestamp: new Date() },
+            { user: users[5], timestamp: new Date() },
+        ],
+        author: users[1],
+        comments: [comments[2], comments[0], comments[1]], // Add comments to the post
+    },
+];
+function findMostLikedPost(posts) {
+    let mostLikedPost = null;
+    let mostLikesCount = 0;
+    for (const post of posts) {
+        const likesCount = post.likes.length;
+        if (likesCount > mostLikesCount) {
+            mostLikedPost = post;
+            mostLikesCount = likesCount;
+        }
+    }
+    const likedBy = mostLikedPost
+        ? mostLikedPost.likes.map((like) => like.user.username)
+        : [];
+    return {
+        post: mostLikedPost,
+        user: mostLikedPost ? mostLikedPost.author : null,
+        likedBy: likedBy,
+        comments: mostLikedPost ? mostLikedPost.comments : [],
+    };
+}
+const result = findMostLikedPost(posts);
+if (result.post && result.user) {
+    console.log(`Most Liked Post: ${result.post.title}`);
+    console.log(`Content of the Post: ${result.post.content}`);
+    console.log(`Number of Likes: ${result.post.likes.length}`);
+    console.log(`Posted by User: ${result.user.username}`);
+    console.log(`Liked by: ${result.likedBy.join(", ")}`);
+    console.log("Comments:");
+    result.comments.forEach((comment) => {
+        console.log(`- ${comment.user.username}: ${comment.content}`);
+    });
+}
+else {
+    console.log("No posts found.");
+}
